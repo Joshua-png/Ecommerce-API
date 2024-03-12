bd1878135ff5aabb18368727d49401e75c0d45d9
Joshua Aryee <profjoshua65@gmail.com>
2024-03-12 05:39:29 +0000

    security changes

--- app.js
+++ app.js
@@ -14,6 +14,11 @@ import OrderRouter from "./routes/OrderRouter.js";
 import { connect } from "./db/connect.js";
 import { errorHandlerMiddleware } from "./middleware/errorHandlerMiddleware.js";
 import cookieParser from "cookie-parser";
+// Limites the amount of requests from a single IP
+import rateLimit from "express-rate-limit";
+import helmet from "helmet";
+import xss from "xss-clean";
+import mongoSanitize from "express-mongo-sanitize";
 // import { v2 as cloudinary } from "cloudinary";
 
 // Initializing an express app
@@ -41,7 +46,19 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 // Middleware
 app.use(express.json()); //Have access to the json data in the req.body
 app.use(cookieParser(process.env.JWT_SECRET));
+app.set("trust proxy", 1);
+app.use(
+  rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 60, // limit each IP to 100 requests per windowMs
+    message: "Too many requests, please try again later.",
+  })
+);
+app.use(helmet());
+app.use(xss());
 app.use(cors());
+app.use(mongoSanitize());
+
 // Public folder middlemware
 app.use(express.static(path.resolve(__dirname, "./public")));
 
@@ -52,20 +69,20 @@ app.use("/api/v1/products", ProductRouter);
 app.use("/api/v1/reviews", ReviewRouter);
 app.use("/api/v1/orders", OrderRouter);
 
-// app.get("*", (req, res) => {
-//   res.sendFile(path.resolve(__dirname, "./public", "index.html"));
-// });
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./public", "index.html"));
+});
 
 const port = process.env.PORT || 3000;
 
-app.get("/api/v1/test", (req, res) => {
-  res.json({ msg: "welcome" });
-});
+// app.get("/api/v1/test", (req, res) => {
+//   res.json({ msg: "welcome" });
+// });
 
-app.get("/api/v1", (req, res) => {
-  console.log(req.signedCookies);
-  res.json({ msg: "welcome" });
-});
+// app.get("/api/v1", (req, res) => {
+//   console.log(req.signedCookies);
+//   res.json({ msg: "welcome" });
+// });
 
 // Not found
 app.use("*", (req, res) => {
